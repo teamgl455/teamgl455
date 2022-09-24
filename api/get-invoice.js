dad4c6adf4d943c0f5ce36de91266621df51d7c3
Hamza <hamzablondon@gmail.com>
2022-09-24 16:55:51 +0100

    test: get-invoice

--- api/get-invoice.js
+++ api/get-invoice.js
@@ -1,4 +1,16 @@
 export default function handler(req, res) {
-    const { name = 'World' } = req.query;
-    return res.send(`Hello ${name}!`);
+    const options = {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: '{"to":"Donor,\nThe Post Building,\n100 Museum St,\nLondon\nWC1A 1PB","from":"C/O Bowline Capital:\n84 Brook St, Mayfair,\nLondon\nW1K 5EH","logo":"https://www.humanpractice.org/wp-content/uploads/2020/01/Logo1-300x159.png","currency":"GBP","due_date":"30/09/22","number":1,"items":[{"name":"Ndururumo High School Library","quantity":1,"unit_cost":500000,"description":"Construction and stocking of secondary school library."},{"name":"Ndururumo High School IT Hub","quantity":1,"unit_cost":125000,"description":"Construction IT hub facility and provision of 100 laptops for students."}]}'
+      };
+      
+      fetch('https://invoice-generator.com/', options)
+        .then(response => res.send(response.json()))
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+    // const { name = 'World' } = req.query;
+    // return res.send(`Hello ${name}!`);
+    
 }  
